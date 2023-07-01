511974e06b0ff01520d35153f9bbe80b0f812e18
Ralph Sébastien Placide <placideralph@gmail.com>
2023-07-01 19:10:46 -0400

    fix: update question generation function

--- lib/features/models/question.dart
+++ lib/features/models/question.dart
@@ -1,12 +1,14 @@
+import 'package:country_quiz/features/models/question_item.dart';
+
 class Question {
-  String answer;
-  String question;
-  List<String> choices;
-  bool hasFlag;
+  final QuestionItem question;
+  final bool hasFlag;
+  final List<QuestionItem> answers;
+  final int currAnswer;
 
   Question({
-    required this.answer,
-    required this.choices,
+    required this.answers,
+    required this.currAnswer,
     required this.hasFlag,
     required this.question,
   });
