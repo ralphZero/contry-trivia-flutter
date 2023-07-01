bc79eb8498095bde7c1f0f692b94638ad8bfb838
Ralph Sébastien Placide <placideralph@gmail.com>
2023-07-01 19:23:38 -0400

    fix: update AppState class

--- lib/features/models/app_state.dart
+++ lib/features/models/app_state.dart
@@ -1,6 +1,4 @@
-import 'dart:convert';
 import 'dart:math';
-
 import 'package:country_quiz/features/controllers/api_call.dart';
 import 'package:country_quiz/features/models/country_api_response.dart';
 import 'package:country_quiz/features/models/question.dart';
@@ -8,37 +6,35 @@ import 'package:country_quiz/features/models/question_item.dart';
 import 'package:flutter/material.dart';
 
 class AppState extends ChangeNotifier {
-  bool isLoading;
-  int started;
-  List<CountryApiResponse>? data;
+  bool isLoading = false;
+  int started = 0;
+  List<CountryApiResponse> data = [];
   Question? currQuestion;
-  int score;
+  int score = 0;
 
-  AppState({
-    this.started = 0,
-    this.data,
-    this.currQuestion,
-    this.score = 0,
-    this.isLoading = false,
-  });
+  // getters
+  bool get getLoadingState => isLoading;
+  int get getScore => score;
+  int get getQuestionCount => started;
+  Question? get getCurrentQuestion => currQuestion;
 
-  initialData() async {
+  void initialData() async {
     isLoading = true;
     data = await fetchCountryData();
     isLoading = false;
     notifyListeners();
   }
 
-  Question generateQuestion() {
+  void generateQuestion() {
     List<QuestionItem> selectedCountryData = [];
 
     for (var i = 0; i < 5; i++) {
-      var randomIndex = Random().nextInt(data!.length - 5);
-      var countryData = data!.elementAt(randomIndex);
+      var randomIndex = Random().nextInt(data.length - 5);
+      var countryData = data.elementAt(randomIndex);
       // if not capital, gengerate another index and get that data from list
       if (countryData.capital.isEmpty) {
-        randomIndex = Random().nextInt(data!.length - 5);
-        countryData = data!.elementAt(randomIndex);
+        randomIndex = Random().nextInt(data.length - 5);
+        countryData = data.elementAt(randomIndex);
       }
       selectedCountryData.add(
         QuestionItem(
@@ -55,11 +51,13 @@ class AppState extends ChangeNotifier {
     var answers = selectedCountryData.sublist(1, selectedCountryData.length);
     var hasFlag = Random().nextInt(10).floor() % 2 == 0 ? false : true;
 
-    return Question(
+    currQuestion = Question(
       answers: answers,
       currAnswer: questions[0].id,
       hasFlag: hasFlag,
       question: questions[0],
     );
+
+    notifyListeners();
   }
 }
