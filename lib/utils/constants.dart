1e1f8b4a79a68f7b59661b1fdb2d641a1b9c49ec
Ralph Sébastien Placide <placideralph@gmail.com>
2023-07-02 01:14:01 -0400

    feat: add new color data

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,6 +5,12 @@ const secondary = Color(0xFF7659E9);
 const tertiary = Color(0xFFFFB800);
 const accentText = Color(0xFF452A11);
 const leadearBoardBtnColor = Color(0x40FFFFFF);
+const optionBgColor = Color(0x425134C6);
+const optionTextColor = Color(0xFF7061C8);
+const optionBgSuccess = Color(0x8710E425);
+const optionTextSuccess = Color(0xFF007713);
+const optionBgFailed = Color(0x8FFF0000);
+const optionTextFailed = Color(0xFF660000);
 
 const pageGradient = RadialGradient(
   center: Alignment(0, 0.5),
